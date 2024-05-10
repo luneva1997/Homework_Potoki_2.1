224bfb8a2c0f45ae0ecc5cbc1d3b3239b635aec3
Yulia <julialuneva97@gmail.com>
2024-05-10 05:14:45 +0300

    First_Commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,15 +4,36 @@ import java.util.concurrent.*;
 public class Main {
     public static final Map<Integer, Integer> sizeToFreq = new HashMap<>();
 
+
+
     public static void main(String[] args){
         final ExecutorService threads = Executors.newFixedThreadPool(1000);
+
+        Thread lieder = new Thread(() -> {
+            while(!Thread.interrupted()) {
+                synchronized (sizeToFreq) {
+                    if (!sizeToFreq.isEmpty()) {
+                        try  {
+                            sizeToFreq.wait();
+                            Map.Entry<Integer, Integer> maxEntry = Collections.max(sizeToFreq.entrySet(), Map.Entry.comparingByValue());
+                            System.out.println("Самое частое количество повторений " + maxEntry.getKey() + " (встретилось " + maxEntry.getValue() + " раз)");
+                        } catch (InterruptedException e) {
+                            return;
+                        }
+                    } else {
+                        System.out.println("Список пустой!");
+                    }
+                }
+            }
+        });
+
+        lieder.start();
 
         for (int j=0; j < 1000; j++) {
             char [] answer;
             int sumR = 0;
 
-
-            try (threads) {
+            try {
                 Future<String> test = threads.submit(() -> generateRoute("RLRFR", 100));
                 answer = test.get().toCharArray();
 
@@ -28,20 +49,22 @@ public class Main {
             synchronized (sizeToFreq) {
                 if (sizeToFreq.containsKey(sumR)) {
                     sizeToFreq.replace(sumR, sizeToFreq.get(sumR) + 1);
+                    sizeToFreq.notify();
                 } else {
                     sizeToFreq.put(sumR, 1);
+                    sizeToFreq.notify();
                 }
 
             }
+
+
         }
 
+
         threads.shutdown();
+        lieder.interrupt();
 
         Map<Integer, Integer> sortedSizeToFreQ = new TreeMap<>(sizeToFreq);
-
-        Map.Entry<Integer, Integer> maxEntry = Collections.max(sortedSizeToFreQ.entrySet(), Map.Entry.comparingByValue());
-
-        System.out.println("Самое частое количество повторений " + maxEntry.getKey() + " (встретилось " + maxEntry.getValue() + " раз)");
 
         for (Map.Entry<Integer,Integer> e: sortedSizeToFreQ.entrySet()) {
             System.out.println("- " + e.getKey() + " (" + e.getValue() + " раз)");
